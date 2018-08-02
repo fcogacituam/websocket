76a21c54a2b65196559d7cbee0652a00188d12c2
Francisco Gacitua <fcogacituam@gmail.com>
2018-08-02 15:02:32 -0400

    aliases

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 	
 	/*JWT*/
-	'Tymon\JWTAuth\Providers\JWTAuthServiceProvider'
+	    Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
     ],
 
     /*
@@ -210,8 +210,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-	'JWTAuth' => 'Tymon\JWTAuth\Facades\JWTAuth',
-	'JWTFactory' => 'Tymon\JWTAuth\Facades\JWTFactory',	
+	    'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+	    'JWTFactory' => 'Tymon\JWTAuth\Facades\JWTFactory',	
 
     ],
 

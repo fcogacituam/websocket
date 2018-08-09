6b1406a1410227e873d1179cb27e7246c8a4ab31
Francisco Gacitua <fcogacituam@gmail.com>
2018-08-09 16:01:24 -0400

    pruebaEvent

--- resources/views/socket.blade.php
+++ resources/views/socket.blade.php
@@ -10,17 +10,6 @@
 <body>
     <h2>Hola desde Sockets</h2>
 
-    <script>
-    
-        import Echo from "laravel-echo"
-
-        window.io = require('socket.io-client');
-
-        window.Echo = new Echo({
-            broadcaster: 'socket.io',
-            host: window.location.hostname + ':6001'
-        });
-
-    </script>
+ 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,7 @@
 <?php
-$router->group(['middleware' => 'jwt-auth'], function () use ($router) {
 
-    $router->get('{class}', function (Illuminate\Http\Request $request, $class) {
-        $controller = app()->make("App\\Http\\Controllers\\" . ucwords($class) . "Controller");
-        return $controller->get($request);
-    });
-
-    $router->get('{class}/{id}', function (Illuminate\Http\Request $request, $class, $id) {
-        $controller = app()->make("App\\Http\\Controllers\\" . ucwords($class) . "Controller");
-        return $controller->find($id);
-    });
-
-    $router->post('{class}', function (Illuminate\Http\Request $request, $class) {
-        $controller = app()->make("App\\Http\\Controllers\\" . ucwords($class) . "Controller");
-        return $controller->post($request);
-    });
-
-    $router->put('{class}/{id}', function (Illuminate\Http\Request $request, $class, $id) {
-        $controller = app()->make("App\\Http\\Controllers\\" . ucwords($class) . "Controller");
-        return $controller->put($request, $id);
-    });
-
-    $router->patch('{class}/{id}', function (Illuminate\Http\Request $request, $class, $id) {
-        $controller = app()->make("App\\Http\\Controllers\\" . ucwords($class) . "Controller");
-        return $controller->patch($request, $id);
-    });
-
-    $router->delete('{class}/{id}', function (Illuminate\Http\Request $request, $class, $id) {
-        $controller = app()->make("App\\Http\\Controllers\\" . ucwords($class) . "Controller");
-        return $controller->delete($request, $id);
-    });
-
-    // EXTRA
-    $router->post('{class}/{method}', function (Illuminate\Http\Request $request, $class, $method) {
-        $controller = app()->make("App\\Http\\Controllers\\" . ucwords($class) . "Controller");
-        return $controller->$method($request);
-    });
-
-});
 
 Route::get("/socket",function(){
    // return view("socket");
-    broadcast(new ExampleEvent);
+    broadcast(new \App\Events\PruebaEvent);
 });

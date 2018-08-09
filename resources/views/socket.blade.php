a410933d0b3a5c477a7b47be3103fcf7dc5fc6d7
Francisco Gacitua <fcogacituam@gmail.com>
2018-08-09 16:40:31 -0400

    llamado a assets

--- resources/views/socket.blade.php
+++ resources/views/socket.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Sockets</title>
 </head>
 <body>
@@ -14,6 +15,6 @@
     </div>
     
 
- 
+ <script src="{{ asset('js/app.js')}}"></script>
 </body>
 </html>

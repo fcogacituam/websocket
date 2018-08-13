a05fa0dc7137faa0a7ec039fb02ad725ff3b5e19
Francisco Gacitua <fcogacituam@gmail.com>
2018-08-13 13:42:01 -0300

    params to kprimasEvent

--- app/Events/KprimasEvent.php
+++ app/Events/KprimasEvent.php
@@ -22,7 +22,7 @@ class KprimasEvent implements ShouldBroadcast
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($pathname, $post, $jwt)
     {
         $this->pathname = $pathname;
         $this->post = $post;

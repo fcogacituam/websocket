e1fa93de3701f74576f001d106358acfc2310bd7
Francisco Gacitua <fcogacituam@gmail.com>
2018-08-20 17:35:24 -0300

    ola

--- resources/assets/js/components/LoginEcore.vue
+++ resources/assets/js/components/LoginEcore.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                  <form action="">
-                    <div class="form-group"><label for="">User: </label><input v-model="userN" type="text" class="form-control" id="userName"></div>
+                    <div class="form-group"><label for="">User: </label><input v-model="mutableUser" type="text" class="form-control" id="userName"></div>
                     <div class="form-group"><label for="">Password: </label><input v-model="pass" type="password" class="form-control" id="passWord"></div>
                     <input v-on:click.prevent="login" type="submit" class="btn btn-primary" value="Entrar">
                 </form>

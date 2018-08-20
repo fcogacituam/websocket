a89f3c8f409ab52e71c272d54b375a89af22ad59
Francisco Gacitua <fcogacituam@gmail.com>
2018-08-20 16:09:55 -0300

    getCookie

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -205,7 +205,3 @@ function sortVersions(arr) {
     return versions.map(a => a.split('.').map(n => +n + 100000).join('.')).sort()
         .map(a => a.split('.').map(n => +n - 100000).join('.'));
 }
-window.Echo.private('user.' + id)
-    .listen('UserEvent', function (data) {
-        console.log("USEREVENTNTNT: ", data);
-    })

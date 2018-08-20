b864211ff3d1112ce088634f3e7a1ad0817c1775
Francisco Gacitua <fcogacituam@gmail.com>
2018-08-20 17:07:29 -0300

    plixito

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,12 +8,13 @@
 </head>
 <body>
     <div id="app">
+        
         <form action="">
             <div class="form-group"><label for="">User: </label><input v-model="userN" type="text" class="form-control"></div>
             <div class="form-group"><label for="">Password: </label><input v-model="pass" type="password" class="form-control"></div>
             <input v-on:click="login()" type="submit" class="btn btn-primary" value="Entrar">
         </form>
+
     </div>
-    
 </body>
 </html>

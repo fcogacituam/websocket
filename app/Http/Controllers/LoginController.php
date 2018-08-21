1f168f74477f8ea201d0b639372f2b2a6ccdc79f
Francisco Gacitua <fcogacituam@gmail.com>
2018-08-21 11:33:42 -0300

    cokies?

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,15 +9,18 @@ class LoginController extends Controller
     public function login(Request $request){
 		// return $request;
 
-		$client = new \GuzzleHttp\Client();
+		$client = new \GuzzleHttp\Client(['cookies'=>true]);
 
         $response = $client->post("https://ecore.builder.widefense.com/api/ecore/public/auth/login", [
             'auth' =>[
 				$request->userName,$request->passWord
 			]
 		]);
+
+		$cookieJar = $client->getConfig('cookies');
+		$cookieJar->toArray();
 		
-		var_dump($response) ;
+		// var_dump($response) ;
 		// return redirect("/socket")->withCookie('id');
 	}
 }

9bd2e56106b7a183681e2c86c75fb0dfdcd9d3a0
Francisco Gacitua <fcogacituam@gmail.com>
2018-08-21 11:24:03 -0300

    guzzle login?

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,16 +7,16 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function login(Request $request){
-		return $request;
+		// return $request;
 
-		// $client = new \GuzzleHttp\Client();
+		$client = new \GuzzleHttp\Client();
 
-        // $response = $client->get("https://ecore.builder.widefense.com/api/ecore/public/auth/login", [
-        //     'auth' =>[
-		// 		$username,$password
-		// 	]
-		// ]);
+        $response = $client->get("https://ecore.builder.widefense.com/api/ecore/public/auth/login", [
+            'auth' =>[
+				$request->userName,$request->passWord
+			]
+		]);
 		
-		// return $response->getBody();
+		return $response->getBody();
 	}
 }
